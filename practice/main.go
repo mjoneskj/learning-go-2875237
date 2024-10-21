64b8ef5de2e5e2fdf1fd5fc49c711a7baf6a4790
mjoneskj <mjones@jackhenry.com>
2024-10-21 15:12:25 -0500

    made change

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
+	theAnswer := 42
+	var result string
+	if theAnswer < 0 {
+		result = "Less than zero"
+
+	} else if theAnswer == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Great than zero"
+	}
+	fmt.Println(result)
 	fmt.Println("Conditional logic")
+
+	if x := -42; x < 0 {
+		result = "Less than zero"
+	} else if x == 0 {
+		result = "Less equals zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
 }
